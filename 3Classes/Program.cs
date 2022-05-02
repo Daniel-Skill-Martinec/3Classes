ffb948abacba5ac16fa26c56c7c90b285bdb12d3
Daniel-Skill-Martinec <danielpokecraft@gmail.com>
2022-05-01 22:24:20 -0400

    Add project files.

---
+++ 3Classes/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _3Classes
+{
+    class Car
+    {
+        string color = "Pink with Black Accents";
+        string Accessories = "Spoiler and Foglights";
+        int maxSpeed = 200;
+
+        static void Main(string[] args)
+        {
+            Car myObj = new Car();
+            Console.WriteLine(myObj.color);
+            Console.WriteLine(myObj.Accessories);
+            Console.WriteLine(myObj.maxSpeed);
+        }
+    }
+}
